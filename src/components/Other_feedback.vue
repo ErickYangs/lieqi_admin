584ee1b8aa7e4396ea82380e2b5f422ebeb67054
ErickYangs <1114341853@qq.com>
2019-01-01 20:39:42 +0800

    ss

--- src/components/Other_feedback.vue
+++ src/components/Other_feedback.vue
@@ -29,10 +29,59 @@
     </div>
     <div class="banner_list">
       <el-tabs v-model="activeName" @tab-click="handleClick">
-        <el-tab-pane :label="feedbackTitle.unread" name="unread">
+        <el-tab-pane :label="unread" name="unread">
+          <el-table
+          @select="sure_feedback"
+            ref="multipleTable"
+            :data="unread_data"
+            border
+            tooltip-effect="dark"
+            style="width: 100%">
+            <el-table-column
+              type="index"
+              width="55"
+              fixed
+              header-align="center"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              label="日期"
+              width="200"
+              prop="date"
+              header-align="center"
+              align="center"
+              show-overflow-tooltip>
+            </el-table-column>
+            <el-table-column
+              prop="question_type"
+              width="140"
+              label="问题分类"
+              header-align="center"
+              align="center"
+              show-overflow-tooltip>
+            </el-table-column>
+            <el-table-column
+              width="160"
+              prop="telphone"
+              header-align="center"
+              align="center"
+              label="联系方式">
+            </el-table-column>
+            <el-table-column width="340" label="链接地址" prop="link" show-overflow-tooltip></el-table-column> 
+            <el-table-column width="380" label="具体描述" prop="descp" show-overflow-tooltip></el-table-column> 
+            <el-table-column
+              type="selection"
+              width="55"
+              header-align="center"
+              align="center"
+              >
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane :label="alread" name="alread">
           <el-table
             ref="multipleTable"
-            :data="dataList"
+            :data="alread_data"
             border
             tooltip-effect="dark"
             style="width: 100%">
@@ -75,9 +124,53 @@
               align="center">
             </el-table-column>
           </el-table>
-        </el-tab-pane>
-        <el-tab-pane :label="feedbackTitle.alread" name="alread">已读反馈</el-tab-pane>
-        <el-tab-pane :label="feedbackTitle.recycle" name="recycle">回收站</el-tab-pane>
+          </el-tab-pane>
+        <el-tab-pane :label="recycle" name="recycle"><el-table
+            ref="multipleTable"
+            :data="recycle_data"
+            border
+            tooltip-effect="dark"
+            style="width: 100%">
+            <el-table-column
+              type="index"
+              width="55"
+              fixed
+              header-align="center"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              label="日期"
+              width="200"
+              prop="date"
+              header-align="center"
+              align="center"
+              show-overflow-tooltip>
+            </el-table-column>
+            <el-table-column
+              prop="question_type"
+              width="140"
+              label="问题分类"
+              header-align="center"
+              align="center"
+              show-overflow-tooltip>
+            </el-table-column>
+            <el-table-column
+              width="160"
+              prop="telphone"
+              header-align="center"
+              align="center"
+              label="联系方式">
+            </el-table-column>
+            <el-table-column width="340" label="链接地址" prop="link" show-overflow-tooltip></el-table-column> 
+            <el-table-column width="380" label="具体描述" prop="descp" show-overflow-tooltip></el-table-column> 
+            <el-table-column
+              type="selection"
+              width="55"
+              header-align="center"
+              align="center">
+            </el-table-column>
+          </el-table>
+          </el-tab-pane>
       </el-tabs>
 
     </div>
@@ -88,18 +181,15 @@
 export default {
   data() {
     return {
-      feedbackTitle: {
-        unread: "未读反馈",
-        alread: "已读反馈",
-        recycle: "回收站"
-      },
       activeName: "unread",
       formInline: {
         date1: "",
         date2: "",
         region: ""
       },
-      dataList: [],
+      unread_data: [],
+      alread_data: [],
+      recycle_data: [],
       multipleSelection: []
     };
   },
@@ -107,15 +197,31 @@ export default {
     get_feedback() {
       this.axios.get("newsList").then(res => {
         let { feedback } = res.data;
-        this.dataList = feedback;
+        this.unread_data = feedback;
       });
     },
-    handleClick(tab, event) {
-      console.log(tab, event);
+    handleClick(tab, event) {},
+    sure_feedback(selection, row) {
+      this.alread_data.push(row);
+      let sure_idx = this.unread_data.findIndex((v, idx, arr) => {
+        return row.id === v.id;
+      });
+      this.unread_data.splice(sure_idx, 1);
     }
   },
   created() {
     this.get_feedback();
+  },
+  computed: {
+    unread() {
+      return "未读反馈(" + this.unread_data.length + ")";
+    },
+    alread() {
+      return "已读反馈(" + this.alread_data.length + ")";
+    },
+    recycle() {
+      return "回收站(" + this.recycle_data.length + ")";
+    }
   }
 };
 </script>
